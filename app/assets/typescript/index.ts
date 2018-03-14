8c6c78929a9a8067f71b1dc38c5b4cc34e32f291
coyotte508 <coyotte508@gmail.com>
2018-03-14 19:34:47 +0100

    Canvas initialization

--- app/assets/typescript/index.ts
+++ app/assets/typescript/index.ts
@@ -1,1 +1,15 @@
 import "../stylesheets/index.scss";
+
+const canvas: HTMLCanvasElement = document.querySelector("#canvas");
+// Initialize the GL context
+const gl = canvas.getContext("webgl");
+
+// Only continue if WebGL is available and working
+if (!gl) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+} else {
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
